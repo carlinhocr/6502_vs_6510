263defaeb24a1267797060374e3b7e49490aee59
carlinhocr <carlos.rienzi@gmail.com>
2023-05-08 15:08:28 -0300

    6510 io pins

---
+++ 008_IO_Data_Pins/008_IO_Data_Pins.ino
@@ -0,0 +1,53 @@
+const char ADDR[] = {22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52}; //char is one byte from -127 to 127
+const char DATA[] = {39,41,43,45,47,49,51,53};
+const char IO[] = {8,9,10,11,12,13};
+
+#define CLOCK 2
+#define READ_WRITE 3
+
+
+void setup() {
+  for (int n=0;n<16;n++){
+    pinMode(ADDR[n], INPUT);
+  }
+   for (int n=0;n<8;n++){
+    pinMode(DATA[n], INPUT);
+  }
+     for (int n=0;n<6;n++){
+    pinMode(IO[n], INPUT);
+  }
+  pinMode(CLOCK, INPUT);
+  pinMode(READ_WRITE, INPUT);
+  // each time that on pin 2 (clock) i receive a HIGH on the rising edge run the onClock function
+  attachInterrupt(digitalPinToInterrupt(CLOCK), onClock, RISING); 
+  Serial.begin(9600);
+}
+
+void onClock (){
+  char output[15];
+  unsigned int address = 0;
+  for (int n=0;n<16;n++){
+    int bit = digitalRead(ADDR[n]) ? 1:0; //? ternary operator if TRUE then 1 else 0
+    Serial.print(bit);
+    address = (address << 1) + bit;
+  }
+  Serial.print("    ");
+  unsigned int data = 0;
+  for (int n=0;n<8;n++){
+    int bit = digitalRead(DATA[n]) ? 1:0; //? ternary operator if TRUE then 1 else 0
+    Serial.print(bit);
+    data = (data << 1) + bit;
+  }
+    Serial.print("    ");
+  unsigned int io = 0;
+  for (int n=0;n<6;n++){
+    int bit = digitalRead(io[n]) ? 1:0; //? ternary operator if TRUE then 1 else 0
+    Serial.print(bit);
+    io = (io << 1) + bit;
+  }
+  sprintf(output, " %04x %04c %02x %02x",address,digitalRead(READ_WRITE)?'r':'W',data,io);
+  Serial.println(output);
+}
+
+void loop() {
+}
