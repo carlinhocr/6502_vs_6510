745408a29fc50b6af106df2d8e9a0444fff630fb
carlinhocr <carlos.rienzi@gmail.com>
2023-05-22 16:25:14 -0300

    io ports completo

--- 007_makerom_6510_IO_Pins_output.py
+++ 007_makerom_6510_IO_Pins_output.py
@@ -4,32 +4,36 @@ rom = bytearray([0xea] *32768) # create an array of 32768 ea
 rom[00] = 0xff # start will all I/O ports as output
 rom[1]  = 0xff # start with all pins on
 
+# 00001  0000             *=$0010
+# 00002  0010  A9 3F                 LDA #$3F
+# 00003  0012  85 00                 STA $0000
+# 00004  0014  A9 3F                 LDA #$3F
+# 00005  0016  85 01                 STA $0001
+# 00006  0018  A9 07                 LDA #$7
+# 00007  001A  85 01                 STA $0001
+# 00008  001C  A9 00                 LDA #$0
+# 00009  001E  85 01                 STA 0001
+# 00010  0020             
+
+
 #load some data on the i/o pins as output, turning bit 0 Off
 # memory position 10 is low byte 0x0a
 rom[10] = 0xa9 # LDA 6502 machine instruction for LDA #$3f # for literal number and $ to encode as hexadecimal
-rom[11] = 0x3f # hexadecimal number 3e 00111110 turn off one bit
-rom[12] = 0x85 # STA to memory expects 3 bytes total (instruction +low address + high address)
+rom[11] = 0x3f # hexadecimal number 3f 00111111 turn off one bit
+rom[12] = 0x85 # STA to memory expects 2 bytes total (instruction +low address)
 rom[13] = 0x00 # low address
 
 #load some data on the i/o pins as output, turning all output pins on
-rom[15] = 0xa9 # LDA 6502 machine instruction for LDA #$3f # for literal number and $ to encode as hexadecimal
-rom[16] = 0x3f # hexadecimal number 3f 00111111
-rom[17] = 0x8d # STA to memory expects 3 bytes total (instruction +low address + high address)
-rom[18] = 0x01 # low address
-rom[19] = 0x00 # high address
+rom[14] = 0xa9 # LDA 6502 machine instruction for LDA #$3f # for literal number and $ to encode as hexadecimal
+rom[15] = 0x3f # hexadecimal number 3f 00111111
+rom[16] = 0x85 # STA to memory expects 3 bytes total (instruction +low address + high address)
+rom[17] = 0x01 # low address
 
 #load some data on the i/o pins as output, turning half on half off
-rom[20] = 0xa9 # LDA 6502 machine instruction for LDA #$3f # for literal number and $ to encode as hexadecimal
-rom[21] = 0x7 # hexadecimal number 3f 00000111
-rom[22] = 0x8d # STA to memory expects 3 bytes total (instruction +low address + high address)
-rom[23] = 0x01 # low address
-rom[24] = 0x00 # high address
-
-#load some data on the i/o pins as output, turning all off
-rom[25] = 0xa9 # LDA 6502 machine instruction for LDA #$3f # for literal number and $ to encode as hexadecimal
-rom[26] = 0x0 # hexadecimal number 3f 00000111
-rom[27] = 0x85 # STA to zero page  memory expects 2 bytes total (instruction +low address as zero page is 256 bytes)
-rom[28] = 0x01 # low address
+rom[18] = 0xa9 # LDA 6502 machine instruction for LDA #$3f # for literal number and $ to encode as hexadecimal
+rom[19] = 0x7 # hexadecimal number 7 00000111
+rom[20] = 0x85 # STA to memory expects 3 bytes total (instruction +low address + high address)
+rom[21] = 0x01 # low address
 
 #tell the procesor to fetch first instrucion at 8010 (0010 of the actual eprom but with A15 enabled it would look at 8010)
 rom[0x7ffa] = 0x0a # low byte that will be read as FFFA
--- 008_6510_IO_Pins_Input.py
+++ 008_6510_IO_Pins_Input.py
@@ -1,23 +1,27 @@
 rom = bytearray([0xea] *32768) # create an array of 32768 ea
 
+# 00001  0000             *=$0010
+# 00002  0010  A9 00                 LDA #$00
+# 00003  0012  85 00                 STA $0000
+# 00004  0014  A5 01                 LDA $01
+# 00005  0016  8D 00 70              STA $7000
+# 00006  0019             
+
 #Load memory address 0 with the vector to set i/o pins as input 00000000 = #$00
 #start program at address 10 we will use address 0 and 1 for the 6510 output pins
-rom[10] = 0xa9 # LDA 6502 machine instruction for LDA #$3f # for literal number and $ to encode as hexadecimal
+rom[10] = 0xa9 # LDA 6502 machine instruction for LDA #$00 # for literal number and $ to encode as hexadecimal
 rom[11] = 0x00 # hexadecimal number 00 00000000
-rom[12] = 0x8d # STA to memory expects 3 bytes total (instruction +low address + high address)
+rom[12] = 0x85 # STA to memory expects 2 bytes total (instruction +low address)
 rom[13] = 0x00 # low address
-rom[14] = 0x00 # high address
-
-#load some data on the i/o pins as output, turning all output pins on
-rom[15] = 0xa5 # LDA 6502 machine instruction for LDA from the zero page memory address 01 mapped to the io ports
-rom[16] = 0x01 # hexadecimal number 3f 00111111
+rom[14] = 0xa5 # LDA 6502 machine instruction for LDA from the zero page memory address 01 mapped to the io ports
+rom[15] = 0x01 # low address
 #write what we read on the i/o port to the 0x7000 address
-rom[17] = 0x8d # STA to memory expects 3 bytes total (instruction +low address + high address)
-rom[18] = 0x00 # low address
-rom[19] = 0x70 # high address
+rom[16] = 0x8d # STA to memory expects 3 bytes total (instruction +low address + high address)
+rom[17] = 0x00 # low address
+rom[18] = 0x70 # high address
 
 #tell the procesor to fetch first instrucion at 8010 (0010 of the actual eprom but with A15 enabled it would look at 8010)
-rom[0x7ffc] = 0x10 # low byte that will be read as FFFC
-rom[0x7ffd] = 0x80 # high byte that will be read as FFFD
+rom[0x7ffa] = 0x0a # low byte that will be read as FFFC
+rom[0x7ffb] = 0x80 # high byte that will be read as FFFD
 with open("rom008.bin","wb") as out_file:
     out_file.write(rom)
